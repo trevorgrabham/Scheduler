0d99d4a26d333d1798944e3aaf1757a2c2cc13f9
tgrabham <tgrabham@sfu.ca>
2021-08-21 20:01:30 -0700

    first commit'

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // work with 3 different objects
+    //      staff objects
+    //          availability
+    //          position
+    //          days working
+    //          potentially preference for morning or night
+    //      day objects
+    //          how many shifts for each time slot
+    //          potentially, whether each shift is good or bad
+    //      days off object
+    //          for each day, a list of the staff who have the day off
+
+    // things to avoid
+    //      clopen
+    //      working outside of availability
+    //      working on days off
+    
+    // things to prioritize
+    //      good shifts to senior staff
+}
